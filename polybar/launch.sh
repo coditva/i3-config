c564929c1612b1d8b9fbdd54c5e9a28cc959b257
Utkarsh Maheshwari <coditva@gmail.com>
2019-04-30 02:55:03 +0530

    Revamp polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the top bar
-polybar --config="$HOME/.config/i3/polybar/config" top &
+# Launch the bars
+polybar --config="$HOME/.config/i3/polybar/config" bottom &
 
 
 echo "Bars launched..."
